3ba8b1e4c0332cbafd5f7c15f5a277f5fd63858d
LoRider615 <thereallorider2233@gmail.com>
2025-09-16 11:35:31 -0700

    Added laser destruction

--- Exhibit L/Assets/Scripts/LasserBehavior.cs
+++ Exhibit L/Assets/Scripts/LasserBehavior.cs
@@ -12,21 +12,23 @@ public class LasserBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.rotation = cameraPos.rotation * Quaternion.Euler(90f, 0, 0);
+        //transform.rotation = cameraPos.rotation * Quaternion.Euler(90f, 0, 0);
 
         // Adjust scale based on raycast
         if (Physics.Raycast(cameraPos.position, cameraPos.forward, out RaycastHit hit, maxDistance, hitObjects))
         {
-            float dist = hit.distance;
-            transform.localScale = new Vector3(transform.localScale.x, dist * 0.15f, transform.localScale.z);
-            transform.position = cameraPos.position + cameraPos.forward  * 0.5f;
-            transform.position = new Vector3(transform.position.x, cameraPos.position.y - 2f, transform.position.z);
+            //float dist = hit.distance;
+            //transform.localScale = new Vector3(transform.localScale.x, dist * 0.15f, transform.localScale.z);
+            //transform.position = cameraPos.position + cameraPos.forward  * 0.5f;
+            //transform.position = new Vector3(transform.position.x, cameraPos.position.y - 2f, transform.position.z);
+            Destroy(hit.collider.gameObject);
+
         }
         else
         {
-            transform.localScale = new Vector3(transform.localScale.x, maxDistance, transform.localScale.z);
+            /*transform.localScale = new Vector3(transform.localScale.x, maxDistance, transform.localScale.z);
             transform.position = cameraPos.position + cameraPos.forward * 0.5f;
-            transform.position= new Vector3(transform.position.x, cameraPos.position.y - 2f, transform.position.z);
+            transform.position= new Vector3(transform.position.x, cameraPos.position.y - 2f, transform.position.z);*/
         }
     }
 }
