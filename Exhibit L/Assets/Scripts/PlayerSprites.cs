1adab093dcd64559482c2aa1a1ba7b89a2b7f383
Ian Iversen-Krampitz <72539343+ianiversenkrampitz@users.noreply.github.com>
2025-12-08 22:31:32 -0800

    fug

---
+++ Exhibit L/Assets/Scripts/PlayerSprites.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerSprites : MonoBehaviour
+{
+    public PlayerController controller;
+    private Renderer rend;
+    public float spriteTime;
+    public bool flipSprite;
+    public bool canFlip;
+    public float speed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        canFlip = true;
+        flipSprite = false;
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (controller.currentSpeed >= 1f)
+        {
+            if (canFlip)
+            {
+                canFlip = false;
+                StartCoroutine(SpriteSwap());
+            }
+        }
+        else
+        {
+            rend.material.mainTextureOffset = new Vector2(0f, 0f);
+            print("static sprite");
+        }
+
+    }
+
+    public IEnumerator SpriteSwap()
+    {
+        flipSprite = !flipSprite;
+
+        if (flipSprite)
+        {
+            rend.material.mainTextureOffset = new Vector2(.25f, 0f);
+            print("sprite 1");
+        }
+        else
+        {
+            rend.material.mainTextureOffset = new Vector2(.5f, 0f);
+            print("sprite 2");
+        }
+        yield return new WaitForSeconds(spriteTime);
+        canFlip = true;
+    }
+}
